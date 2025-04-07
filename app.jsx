e734b9955bbc85d613ef33747c1e264ff8b0879e
Henok <henokassefa494@gmail.com>
2025-04-06 23:58:25 -0400

    Component

---
+++ app.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+
+import React from 'react';
+import Greeting from './components/Greeting';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Greeting />
+    </>
+  );
+}
+
+export default App;
